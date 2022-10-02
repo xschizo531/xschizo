ff90b425b5b750810c16ff5d6e28b1060a9c220c
NSH531 <NSH531@GMAIL.COM>
2022-10-02 16:51:34 +0300

    Update ml66.py

--- ml66.py
+++ ml66.py
@@ -130,7 +130,7 @@ def wsgi_app():
 if __name__ == '__main__':
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     
     PORT = 5310
 
